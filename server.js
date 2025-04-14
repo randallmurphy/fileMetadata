d137ebe98aeb94b0d88be5c0d29f73d97419fc92
randall murphy <rmurphy@perseverenow.org>
2025-04-14 11:08:40 -0500

    try 4

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const upload = multer({ dest: 'uploads/' });
 
 app.use(express.json()); // Use built-in JSON parser
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public/index.html'))); // Serve static files
+app.use(express.static(path.join(__dirname, 'public/'))); // Serve static files
 
 // File upload endpoint
 // app.post('/api/upload', upload.single('upfile'), (req, res) => {
